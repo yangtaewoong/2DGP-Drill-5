d1cbcefc62b5a63ba035c61e186f23f06e23ba93
yangtaewoong <tom0323@naver.com>
2024-09-30 14:45:22 +0900

    끝

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+#  높이 159 * i
 
 open_canvas()
 ground = load_image('TUK_GROUND.png')
@@ -7,7 +7,7 @@ character = load_image('run_animation.png')
 
 
 def handle_events():
-    global running,dir
+    global running,dirx,diry
 
     events = get_events()
     for event in events:
@@ -15,31 +15,54 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dirx += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dirx -= 1
+            elif event.key == SDLK_UP:
+                diry += 1
+            elif event.key == SDLK_DOWN:
+                diry -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                dirx -= 1
             elif event.key == SDLK_LEFT:
-                dir += 1
-
+                dirx += 1
+            elif event.key == SDLK_UP:
+                diry -= 1
+            elif event.key == SDLK_DOWN:
+                diry += 1
 
 running = True
 x = 800 // 2
+y = 100
 frame = 0
-dir = 0
+dirx = 0
+diry = 0
+
+def draw_character():
+    global dirx,diry
+    if dirx > 0:
+        character.clip_draw(frame * 95, 159, 95, 159, x, y,50,100)
+    elif dirx < 0:
+        character.clip_draw(frame * 95, 159*2, 95, 159, x, y,50,100)
+    elif diry > 0:
+        character.clip_draw(frame * 95, 0, 95, 159, x, y,50,100)
+    elif diry < 0:
+        character.clip_draw(frame * 95, 159*3, 95, 159, x, y,50,100)
+    else:
+        character.clip_draw(frame * 95, 159 * 3, 95, 159, x, y,50,100)
 
 while running:
     clear_canvas()
-    ground.draw(400, 30)
-    character.clip_draw(frame * 96, 0, 100, 100, x, 100)
+    ground.draw(400, 100)
+    draw_character()
     update_canvas()
     handle_events()
     frame = (frame + 1) % 12
-    x += dir * 5
+    x = max(25, min(800 - 25, x + dirx * 5))
+    y = max(50, min(600 - 50, y + diry * 5))
     delay(0.05)
 
 close_canvas()
