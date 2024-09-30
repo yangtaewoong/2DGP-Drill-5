416027ba5ce61c465e8650aa7789b10a974bc617
yangtaewoong <tom0323@naver.com>
2024-09-30 14:14:43 +0900

    배경과 캐릭터 띄우기 완료

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -2,28 +2,45 @@ from pico2d import *
 
 
 open_canvas()
-grass = load_image('grass.png')
-character = load_image('animation_sheet.png')
+ground = load_image('TUK_GROUND.png')
+character = load_image('run_animation.png')
 
 
 def handle_events():
-    global running
-
-    # fill here
+    global running,dir
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # fill here
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
 
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
 
-# fill here
-
+while running:
+    clear_canvas()
+    ground.draw(400, 30)
+    character.clip_draw(frame * 96, 0, 100, 100, x, 100)
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 12
+    x += dir * 5
+    delay(0.05)
 
 close_canvas()
 
